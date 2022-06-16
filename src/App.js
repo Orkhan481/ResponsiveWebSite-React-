ecfdffc8f1148e6282dc81e8df106025e5331d62
Orkhan481 <orxan17022002@gmail.com>
2022-06-17 01:58:42 +0400

    Test section finished

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavbarAndHeader from './components/NavbarAndHeader';
 import ProcessManagement from './components/ProcessManagement';
+import TestSection from './components/TestSection';
 
 function App() {
   return (
     <div className="App">
      < NavbarAndHeader />
      <ProcessManagement />
+     <TestSection />
     </div>
   );
 }
---
+++ src/components/TestSection.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCircleArrowRight} from '@fortawesome/free-solid-svg-icons';
+const TestSection = () => {
+  return (
+    <div className='test-section'>
+        <div className="titles">
+        <h3>Test Yönetimiyle Neler Sağlıyoruz?</h3>
+        <p>IoT Destekli Çözümler</p>
+        </div>
+        <div className="row cards">
+
+            <div className=" card-item col-md-2 col-xs-12  d-flex">
+            <p>Yazılım Kalitesini Arttırıyoruz</p>
+            </div>
+
+            <div className=" card-item col-md-1 col-xs-12  d-flex">
+            <FontAwesomeIcon icon={faCircleArrowRight} size="2x"/>
+            </div>
+
+            <div className=" card-item col-md-2 col-xs-12  d-flex">
+            <p>Yazılım Kalitesini Arttırıyoruz</p>
+            </div>
+
+            <div className=" card-item col-md-1 col-xs-12  d-flex"> 
+            <FontAwesomeIcon icon={faCircleArrowRight} size="2x"/>
+            </div>
+
+            <div className=" card-item col-md-2 col-xs-12  d-flex">
+            <p>Yazılım Kalitesini Arttırıyoruz</p>
+            </div>
+
+            <div className=" card-item col-md-1 col-xs-12  d-flex">
+            <FontAwesomeIcon icon={faCircleArrowRight} size="2x"/>
+            </div>
+            
+
+            <div className=" card-item col-md-2 col-xs-12  d-flex">
+            <p>Yazılım Kalitesini Arttırıyoruz</p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default TestSection
